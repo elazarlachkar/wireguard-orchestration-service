92efde91941906ceffdaaf53377293bda8fde743
Elazar <elazar098@gmail.com>
2022-10-03 11:54:09 +0300

    fix: Handling PeerInfo validation when the handshake process wasn't complete

--- wireguard_orchestration/entities.py
+++ wireguard_orchestration/entities.py
@@ -5,8 +5,8 @@ import pydantic
 
 
 class PeerInfo(pydantic.BaseModel):
-    display_name: str
-    public_key: bytes
+    display_name: typing.Optional[str]  # Could be None if the peer handshake process wasn't complete
+    public_key: typing.Optional[bytes]  # Could be None if the peer handshake process wasn't complete
     virtual_ip: ipaddress.IPv4Address
 
 
